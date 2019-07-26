05374e09b5fc202b294bb52a1a3072565ffa554b
jorge machado <jrms_jorgesilva@hotmail.com>
2019-07-26 18:22:03 -0300

    Added firebase, firebase-cli tools, and configurations for database

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'react-dates/lib/css/_datepicker.css';
+import './firebase/firebase';
 
 const store = configureStore();
 const state = store.getState();
