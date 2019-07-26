b4b2ab5ca1652435d8ba528df0949040a26b72a9
jorge machado <jrms_jorgesilva@hotmail.com>
2019-07-25 23:40:29 -0300

    modificações no componente ExpenseListitem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt}) => {
     return (
@@ -7,7 +9,10 @@ const ExpenseListItem = ({ id, description, amount, createdAt}) => {
             <Link to = {`/edit/${id}`} >
               <h3>{description}</h3>
             </Link>
-            <p>{amount} - {createdAt}</p>
+            <p>
+            {numeral(amount / 100).format('$0,0.00')} 
+            -
+            {moment(createdAt).format('MM Do YYYY')}</p>
         </div>
     );
 }
